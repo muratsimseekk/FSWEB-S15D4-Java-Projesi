de4d7f955eb278cb38d82607b7f58b817f7e1af1
Murat Simsek <muratsimseekk@gmail.com>
2024-01-29 01:24:37 +0300

    solves first problem

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,6 +13,33 @@ public class Main {
     }
 
     public static boolean checkForPalindrome(String text) {
+
+        System.out.println("Initial State : " + text);
+        text = text.replaceAll("[.,'?\\-\\s]","").toLowerCase();
+       Stack<Character> stackOriginal = new Stack<>();
+
+       var stackTemp = new Stack<>();
+       var stackReversed = new Stack<>();
+
+       var chars = text.toCharArray();
+       for (var c : chars){
+           stackOriginal.push(c);
+           stackTemp.push(c);
+       }
+
+       for (char c : chars){
+           Object charTemp = stackTemp.pop();
+           stackReversed.push((Character)charTemp);
+       }
+
+
+        System.out.println("Reversed :" +stackReversed);
+        System.out.println("Original : " + stackOriginal);
+
+        if (stackOriginal.equals(stackReversed)){
+            return true;
+        }
         return false;
+
     }
 }
